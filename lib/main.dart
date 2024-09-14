40eb8c5bb43540a5abe070c5b6fa72fbc3f81a0f
muhammad rio <muhammad.rioperm@gmail.com>
2024-09-14 18:36:51 +0700

    Setup theme and assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/data/core/config/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
